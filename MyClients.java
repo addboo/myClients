dac5ba58ee28d670ebe645cc260dc99ae1e85eca
Andreas Bourakis <bourakis@gmail.com>
2022-01-27 13:25:41 +0200

    Update MyClients.java

--- MyClients.java
+++ MyClients.java
@@ -2,6 +2,9 @@
 package myclients;
 
 import java.util.Scanner; 
+import java.io.File;  // Import the File class
+import java.io.FileWriter;
+import java.io.IOException;  // Import the IOException class to handle errorsimport java.io.File; 
 
 public class MyClients 
 {
@@ -42,7 +45,39 @@ public class MyClients
     
     public static void newClient()
     {
-        System.out.println("\n -new Client \n");
+        System.out.println("\nΕΙΣΑΓΩΓΗ ΝΕΟΥ ΠΕΛΑΤΗ");
+        System.out.println("--------------------");
+        
+        Scanner in = new Scanner(System.in);
+        
+        System.out.print("Όνομα : ");
+        
+        String first_name = in.nextLine();
+        
+        System.out.print("Επώνυμο : ");
+
+        String last_name = in.nextLine();
+
+        System.out.print("E-mail : ");
+
+        String email = in.nextLine();
+        
+        
+        try 
+        {
+          FileWriter myWriter = new FileWriter("myclients.csv");
+
+          myWriter.write(first_name + ',' + last_name + ',' + email);
+          myWriter.close();
+
+          System.out.println("\n\nSuccessfully wrote to the file.\n\n");
+        } 
+        catch (IOException e) 
+        {
+          System.out.println("\n\nERROR: An error occurred.\n\n");
+          e.printStackTrace();
+        }
+
     }
 
     public static void listClients()
