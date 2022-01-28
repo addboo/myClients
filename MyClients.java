4f2d709addd49a23f540f226a080b8d834d7c022
Andreas Bourakis <bourakis@gmail.com>
2022-01-28 12:18:55 +0200

    Update MyClients.java

--- MyClients.java
+++ MyClients.java
@@ -4,7 +4,13 @@ package myclients;
 import java.util.Scanner; 
 import java.io.File;  // Import the File class
 import java.io.FileWriter;
+import java.io.FileReader;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.IOException;  // Import the IOException class to handle errorsimport java.io.File; 
+import java.io.FileNotFoundException; 
+import java.util.Properties;
+
 
 public class MyClients 
 {
@@ -77,17 +83,68 @@ public class MyClients
           System.out.println("\n\nERROR: An error occurred.\n\n");
           e.printStackTrace();
         }
-
     }
 
     public static void listClients()
     {
-        System.out.println("\n -Lists Clients \n");
+        System.out.println("\n\nLists Clients");
+        System.out.println("------------- \n");
+        
+        try 
+        {
+            File myObj = new File("myclients.csv");
+            Scanner myReader = new Scanner(myObj);
+      
+            while (myReader.hasNextLine()) 
+            {
+                String data = myReader.nextLine();
+                System.out.println(data);
+            }
+      
+            myReader.close();
+        } 
+        catch (FileNotFoundException e) 
+        {
+            System.out.println("An error occurred.");
+            e.printStackTrace();
+        }
+        
+        System.out.println("\n");
     }
     
     public static void deleteClient()
     {
-        System.out.println("\n -Delete Client \n");
+        System.out.println("\nΔΙΑΓΡΑΦΗ ΠΕΛΑΤΗ");
+        System.out.println("--------------------");
+        
+        Scanner in = new Scanner(System.in);
+        
+        System.out.print("Eπώνυμο : ");
+        
+        String last_name = in.nextLine();
+        
+        File inputFile = new File("myclients.csv");
+        File tempFile = new File("myclients.temp");
+
+        BufferedReader reader = new BufferedReader(new FileReader(inputFile));
+        BufferedWriter writer = new BufferedWriter(new FileWriter(tempFile));
+
+        String currentLine;
+
+        while((currentLine = reader.readLine()) != null) 
+        {
+            // trim newline when comparing with lineToRemove
+            String trimmedLine = currentLine.trim();
+            
+            if(trimmedLine.equals(last_name)) continue;
+            writer.write(currentLine + System.getProperty("line.separator"));
+        }
+        
+        writer.close(); 
+        reader.close(); 
+        
+        boolean successful = tempFile.renameTo(inputFile);
+        
+        System.out.println("\n");
     }
-    
 }
